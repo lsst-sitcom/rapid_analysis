c043c141fddb6b198059864b96d0a906f25654c1
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2020-02-18 18:19:41 -0600

    Add text display and output as quickLookExp

--- python/lsst/rapid/analysis/monitoring.py
+++ python/lsst/rapid/analysis/monitoring.py
@@ -19,6 +19,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
+import numpy as np
 import lsst.daf.persistence as dafPersist
 from .bestEffort import BestEffortIsr
 from time import sleep
@@ -34,9 +36,10 @@ class Monitor():
         """"""
         self.repoDir = repoDir
         self.display = fireflyDisplay
-        self.butler = dafPersist.Butler(repoDir)
         self.bestEffort = BestEffortIsr(repoDir)
         self.writePostIsrImages = None
+        outpath = os.path.join(repoDir, 'rerun/quickLook')
+        self.butler = dafPersist.Butler(inputs=repoDir, outputs=outpath)
 
     def _getLatestExpId(self):
         return sorted(self.butler.queryMetadata('raw', 'expId'))[-1]
@@ -49,6 +52,52 @@ class Monitor():
         dayObs, seqNum = self._getDayObsSeqNumFromExpId(expId)
         dataId = {'dayObs': dayObs, 'seqNum': seqNum}
         return dataId, expId
+
+    def _calcImageStats(self, exp):
+        elements = []
+        median = np.median(exp.image.array)
+        elements.append(f"Median={median:.2f}")
+        mean = np.mean(exp.image.array)
+        # elements.append(f"Median={median:.2f}")
+        elements.append(f"Mean={mean:.2f}")
+
+        return elements
+
+    def _makeImageInfoText(self, dataId, exp, asList=False):
+        elements = []
+
+        imageType = self.butler.queryMetadata('raw', 'imageType', dataId)[0]
+        obj = None
+        if imageType.upper() not in ['BIAS', 'DARK', 'FLAT']:
+            try:
+                obj = self.butler.queryMetadata('raw', 'OBJECT', dataId)[0]
+                obj = obj.replace(' ', '')
+            except Exception:
+                pass
+
+        for k, v in dataId.items():  # dataId done per line for vertical display
+            elements.append(f"{k}:{v}")
+
+        if obj:
+            elements.append(f"{obj}")
+        else:
+            elements.append(f"{imageType}")
+
+        elements.extend(self._calcImageStats(exp))
+
+        if asList:
+            return elements
+        return " ".join([e for e in elements])
+
+    def _printImageInfo(self, elements):
+        size = 3
+        top = 3850  # just under title for size=3
+        xnom = -600  # 0 is the left edge of the image
+        vSpacing = 100  # about right for size=3, can make f(size) if needed
+        for i, item in enumerate(elements):
+            y = top - (i*vSpacing)
+            x = xnom + (size * 18.5 * len(item)//2)
+            self.display.dot(str(item), x, y, size, ctype='red', fontFamily="courier")
 
     def run(self, durationInSeconds=-1):
 
@@ -71,11 +120,13 @@ class Monitor():
                 exp = self.butler.get('raw', **dataId)
 
             if self.writePostIsrImages:
-                self.butler.put(exp, "postISRCCD", dataId)
+                self.butler.put(exp, "quickLookExp", dataId)
 
             print(f"Displaying {dataId}...")
+            imageInfoText = self._makeImageInfoText(dataId, exp, asList=True)
             self.display.mtv(exp, title=str(dataId))
             self.display.scale('asinh', 'zscale')
+            self._printImageInfo(imageInfoText)
             lastDisplayed = expId
 
         return
