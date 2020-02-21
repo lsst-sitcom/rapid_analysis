81facd1ed275e7c046636d7cb4d9e8926c4c4f4f
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2020-02-20 20:34:36 -0600

    Forgot main star PSF!

--- python/lsst/rapid/analysis/psfMeasurement.py
+++ python/lsst/rapid/analysis/psfMeasurement.py
@@ -140,11 +140,14 @@ class QuickFrameMeasurement():
         x = objData[brightestObjSrcNum]['xCentroid']
         y = objData[brightestObjSrcNum]['yCentroid']
         brightestObjCentroid = (x, y)
+        xx = objData[brightestObjSrcNum]['xx']
+        yy = objData[brightestObjSrcNum]['yy']
         brightestObjApFlux70 = objData[brightestObjSrcNum]['apFlux70']
         brightestObjApFlux25 = objData[brightestObjSrcNum]['apFlux25']
 
         exp.image += median  # put background back in
         return pipeBase.Struct(brightestObjCentroid=brightestObjCentroid,
+                               brightestObjCentroidXxYy=(xx, yy),
                                brightestObjApFlux70=brightestObjApFlux70,
                                brightestObjApFlux25=brightestObjApFlux25,
                                medianPsf=medianPsf,)
