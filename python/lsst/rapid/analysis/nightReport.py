b8694c9ff9219866b72c7382aa39831671d054e0
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2020-02-26 17:38:19 -0600

    Add night report text printing
    Improve coloring and markers
    Add a real, translated ObservationInfo to each seqNum
    Allow reloading during the night without rescanning data we already have
    Add colour and marker map to class state

--- python/lsst/rapid/analysis/nightReport.py
+++ python/lsst/rapid/analysis/nightReport.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from dataclasses import dataclass
+import logging
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
@@ -28,36 +31,69 @@ from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 
 import lsst.daf.persistence as dafPersist
+from astro_metadata_translator import ObservationInfo
 
 CALIB_VALUES = ['FlatField position', 'Park position']
+N_STARS_PER_SYMBOL = 9
+MARKER_SEQUENCE = ['*', 'o', "D", 'P', 'v', "^", 's']
 SOUTHPOLESTAR = 'HD 185975'
+
+
+@dataclass
+class ColorAndMarker:
+    '''Class for holding colors and marker symbols'''
+    color: list
+    marker: str = '*'
 
 
 class NightReporter():
 
     def __init__(self, repoDir, dayObs, deferLoadingData=False):
+        self._supressAstroMetadataTranslatorWarnings()
+
         self.butler = dafPersist.Butler(repoDir)
         self.dayObs = dayObs
         self.data = {}
+        self.stars = None
+        self.cMap = None
         self.auxTelLocation = EarthLocation(lat=-30.244639*u.deg, lon=-70.749417*u.deg, height=2663*u.m)
         if not deferLoadingData:
             self.rebuild()
 
+    def _supressAstroMetadataTranslatorWarnings(self):
+        """NB: must be called early"""
+        logging.basicConfig()
+        _astroLogger = logging.getLogger("lsst.obs.lsst.translators.latiss")
+        _astroLogger.setLevel(logging.ERROR)
+
     def rebuild(self, dayObs=None):
+        """Reload new observations, or load a different night"""
         dayToUse = self.dayObs
         if dayObs:
+            # new day, so blow away old data
+            # as scraping skips seqNums we've loaded!
+            if dayObs != self.dayObs:
+                self.data = {}
+                self.dayObs = dayObs
             dayToUse = dayObs
-        self.data = self._scrapeData(dayToUse)
+        self._scrapeData(dayToUse)
+        self.stars = self.getObservedObjects()
+        self.cMap = self.makeStarColorAndMarkerMap(self.stars)
 
     def _scrapeData(self, dayObs):
-        # TODO: add skipping files we already have
-        _data = {}
+        """Load data into self.data skipping as necessary. Don't call directly!
+
+        Don't call directly as the rebuild() function zeros out data for when
+        it's a new dayObs."""
         seqNums = sorted(self.butler.queryMetadata('raw', 'seqNum', dayObs=dayObs))
         for seqNum in sorted(seqNums):
+            if seqNum in self.data.keys():
+                print(f"skipped {seqNum}")
+                continue
             md = self.butler.get('raw_md', dayObs=dayObs, seqNum=seqNum)
-            _data[seqNum] = md.toDict()
+            self.data[seqNum] = md.toDict()
+            self.data[seqNum]['ObservationInfo'] = ObservationInfo(md)
         print(f"Loaded data for seqNums {sorted(seqNums)[0]} to {sorted(seqNums)[-1]}")
-        return _data
 
     def getUniqueValuesForKey(self, key, ignoreCalibs=True):
         values = []
@@ -81,13 +117,12 @@ class NightReporter():
         ax.set_rlim(0, 90)
         return ax
 
-    def makePolarPlotForObjects(self, objects, colorMap=None, withLines=True):
+    def makePolarPlotForObjects(self, objects=None, withLines=True):
+        if not objects:
+            objects = self.stars
         objects = self._safeListArg(objects)
-        _ = plt.figure(figsize=(10, 10))
 
-        marker = "*"
-        if withLines:
-            marker += '-'
+        _ = plt.figure(figsize=(10, 10))
 
         for i, obj in enumerate(objects):
             azs = self.getAllValuesForKVPair('AZSTART', ("OBJECT", obj))
@@ -96,9 +131,11 @@ class NightReporter():
             if len(azs) == 0:
                 print(f"WARNING: found no alt/az data for {obj}")
             zens = [90 - el for el in els]
-            color = None
-            if colorMap:
-                color = colorMap[obj]
+            color = self.cMap[obj].color
+            marker = self.cMap[obj].marker
+            if withLines:
+                marker += '-'
+
             ax = self._makePolarPlot(azs, zens, marker=marker, title=None, makeFig=False,
                                      color=color, objName=obj)
         lgnd = ax.legend(bbox_to_anchor=(1.05, 1), prop={'size': 15}, loc='upper left')
@@ -119,12 +156,14 @@ class NightReporter():
         return ret
 
     @staticmethod
-    def makeStarColorMapDict(stars):
-        colorMap = {}
-        colors = cm.rainbow(np.linspace(0, 1, len(stars)))
+    def makeStarColorAndMarkerMap(stars):
+        markerMap = {}
+        colors = cm.rainbow(np.linspace(0, 1, N_STARS_PER_SYMBOL))
         for i, star in enumerate(stars):
-            colorMap[star] = colors[i]
-        return colorMap
+            markerIndex = i//(N_STARS_PER_SYMBOL)
+            colorIndex = i%(N_STARS_PER_SYMBOL)
+            markerMap[star] = ColorAndMarker(colors[colorIndex], MARKER_SEQUENCE[markerIndex])
+        return markerMap
 
     def getObjectValues(self, key, objName):
         return self.getAllValuesForKVPair(key, ('OBJECT', objName), uniqueOnly=False)
@@ -152,7 +191,7 @@ class NightReporter():
 
     def plotPerObjectAirMass(self, objects=None, versusMjd=True):
         if not objects:
-            objects = self.getObservedObjects()
+            objects = self.stars
 
         objects = self._safeListArg(objects)
 
@@ -164,12 +203,47 @@ class NightReporter():
         _ = plt.figure(figsize=(10, 6))
         for star in objects:
             ams, times = np.asarray(airMasses[star])[:, 0], np.asarray(airMasses[star])[:, 1]
+            color = self.cMap[star].color
+            marker = self.cMap[star].marker
             if versusMjd:
-                plt.plot(times, ams, '*', color=colorMap[star], label=star, ms=10)
+                plt.plot(times, ams, '*', color=color, marker=marker, label=star, ms=10)
             else:
-                plt.plot(ams, '*', color=colorMap[star], label=star, ms=10)
+                plt.plot(ams, '*', color=color, marker=marker, label=star, ms=10)
         plt.ylabel('Airmass', fontsize=20)
         _ = plt.legend(bbox_to_anchor=(1, 1.025), prop={'size': 15}, loc='upper left')
+
+    def printObsTable(self, imageType=None, tailNumber=0):
+        """Print a table of the days observations.
+
+        Parameters
+        ----------
+        imageType : str
+            Only consider images with this image type
+        tailNumber : int
+            Only print out the last n entries in the night
+        """
+        lines = []
+        if not imageType:
+            seqNums = self.data.keys()
+        else:
+            seqNums = [s for s in self.data.keys()
+                       if self.data[s]['ObservationInfo'].observation_type == imageType]
+
+        seqNums = sorted(seqNums)
+        for i, seqNum in enumerate(seqNums):
+            expTime = self.data[seqNum]['EXPTIME']
+            filt = self.data[seqNum]['ObservationInfo'].physical_filter
+            imageType = self.data[seqNum]['ObservationInfo'].observation_type
+            d1 = self.data[seqNum]['ObservationInfo'].datetime_begin
+            if i == 0:
+                d0 = d1
+            dt = (d1-d0)
+            d0 = d1
+            timeOfDay = d1.isot.split('T')[1]
+            lines.append(f'{seqNum:4} {imageType:5} {timeOfDay} {filt:25} {dt.sec:6.1f}  {expTime:2.2f}')
+
+        for line in lines[-tailNumber:]:
+            print(line)
 
     @staticmethod
     def _safeListArg(arg):
@@ -184,5 +258,5 @@ if __name__ == '__main__':
     nightReporter = NightReporter(repodir, "2020-02-20")
     stars = nightReporter.getUniqueValuesForKey('OBJECT')
     import ipdb as pdb; pdb.set_trace()
-    colorMap = nightReporter.makeStarColorMapDict(stars)
+    colorMap = nightReporter.makeStarColorAndMarkerMap(stars)
     nightReporter.makePolarPlotForObjects(stars, colorMap, withLines=False)
