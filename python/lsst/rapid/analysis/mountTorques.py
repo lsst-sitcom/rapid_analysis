2db319fb4e318a292afee44b4f1ea04db9e61ebf
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2021-07-22 10:25:45 -0500

    Refactor RubinTV services completely.
    Now have a single top level automatic isr runner, and TV channel
    services which sit below waiting for their data products to arrive.
    
    This refactoring will allow Gen3 migration to be much easier, as well
    as removing duplicate processing, and allowing for easy creation of
    summit daemons to be run on the newly provisioned dm-bastion machine
    at the summit.

---
+++ python/lsst/rapid/analysis/mountTorques.py
@@ -0,0 +1,186 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.time import Time
+import asyncio
+
+try:
+    from lsst_efd_client import merge_packed_time_series as mpts
+    HAS_EFD_CLIENT = True
+except ImportError:
+    HAS_EFD_CLIENT = False
+
+GOOD_IMAGE_TYPES = ['OBJECT', 'SKYEXP', 'ENGTEST']
+
+
+def getData(client, dataSeries, startTime, endTime):
+    """A synchronous get of the data from the EFD
+
+    This exists so that the top level functions don't all have to be async def
+    """
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(client.select_time_series(dataSeries, ['*'], startTime, endTime))
+
+
+def plotMountTracking(dataId, butler, client, figure, saveFilename, butlerGeneration, logger):
+    """Queries EFD for given exposure and checks if there were tracking errors.
+        Parameters
+        ----------
+        XXX Write the docs
+
+        Returns
+        -------
+        None
+    """
+    # lsst-efd-client is not a required import at the top here, but is
+    # implicitly required as a client is passed into this function so is not
+    # rechecked here.
+
+    start = time.time()
+
+    if butlerGeneration == 3:
+        expId = dataId['expId']
+        mData = butler.get('raw.metadata', detector=0, exposure=expId)
+        dataIdString = str(expId)
+    else:
+        mData = butler.get('raw_md', **dataId)
+        dataIdString = f"{dataId['dayObs']}-{dataId['seqNum']}"
+
+    imgType = mData['IMGTYPE']
+    tStart = mData['DATE-BEG']
+    tEnd = mData['DATE-END']
+    elevation = mData['ELSTART']
+    azimuth = mData['AZSTART']
+    exptime = mData['EXPTIME']
+    logger.debug(f"dataId={dataIdString}, imgType={imgType}, Times={tStart}, {tEnd}")
+
+    if imgType not in GOOD_IMAGE_TYPES:
+        logger.info(f'Skipping image type {imgType}')
+        return False
+    if exptime < 1.99:
+        logger.info('Skipping sub 2s expsoure')
+        return False
+
+    end = time.time()
+    elapsed = end-start
+    logger.debug(f"Elapsed time for butler query = {elapsed}")
+
+    start = time.time()
+    # Time base in the EFD is still a big mess.  Although these times are in
+    # UTC, it is necessary to tell the code they are in TAI. Then it is
+    # necessary to tell the merge_packed_time_series to use UTC.
+    # After doing all of this, there is still a 2 second offset,
+    # which is discussed in JIRA ticket DM-29243, but not understood.
+
+    t_start = Time(tStart, scale='tai')
+    t_end = Time(tEnd, scale='tai')
+    logger.debug(f"Tstart = {t_start.isot}, Tend = {t_end.isot}")
+
+    mount_position = getData(client, "lsst.sal.ATMCS.mount_AzEl_Encoders", t_start, t_end)
+    nasmyth_position = getData(client, "lsst.sal.ATMCS.mount_Nasmyth_Encoders", t_start, t_end)
+    torques = getData(client, "lsst.sal.ATMCS.measuredTorque", t_start, t_end)
+    logger.debug("Length of time series", len(mount_position))
+
+    az = mpts(mount_position, 'azimuthCalculatedAngle', stride=1, internal_time_scale="utc")
+    el = mpts(mount_position, 'elevationCalculatedAngle', stride=1, internal_time_scale="utc")
+    rot = mpts(nasmyth_position, 'nasmyth2CalculatedAngle', stride=1, internal_time_scale="utc")
+    az_torque_1 = mpts(torques, 'azimuthMotor1Torque', stride=1, internal_time_scale="utc")
+    az_torque_2 = mpts(torques, 'azimuthMotor2Torque', stride=1, internal_time_scale="utc")
+    el_torque = mpts(torques, 'elevationMotorTorque', stride=1, internal_time_scale="utc")
+    rot_torque = mpts(torques, 'nasmyth2MotorTorque', stride=1, internal_time_scale="utc")
+
+    end = time.time()
+    elapsed = end-start
+    logger.debug(f"Elapsed time to get the data = {elapsed}")
+    start = time.time()
+
+    # Calculate the tracking errors
+    az_vals = np.array(az.values.tolist())[:, 0]
+    el_vals = np.array(el.values.tolist())[:, 0]
+    rot_vals = np.array(rot.values.tolist())[:, 0]
+    times = np.array(az.values.tolist())[:, 1]
+    logger.debug("Length of packed time series", len(az_vals))
+
+    # Fit with a linear
+    az_fit = np.polyfit(times, az_vals, 1)
+    el_fit = np.polyfit(times, el_vals, 1)
+    rot_fit = np.polyfit(times, rot_vals, 1)
+    az_model = az_fit[0] * times + az_fit[1]
+    el_model = el_fit[0] * times + el_fit[1]
+    rot_model = rot_fit[0] * times + rot_fit[1]
+
+    # Errors in arcseconds
+    az_error = (az_vals - az_model) * 3600
+    el_error = (el_vals - el_model) * 3600
+    rot_error = (rot_vals - rot_model) * 3600
+
+    # Calculate RMS
+    az_rms = np.sqrt(np.mean(az_error * az_error))
+    el_rms = np.sqrt(np.mean(el_error * el_error))
+    rot_rms = np.sqrt(np.mean(rot_error * rot_error))
+
+    end = time.time()
+    elapsed = end-start
+    logger.debug(f"Elapsed time for error calculations = {elapsed}")
+    start = time.time()
+
+    # Plotting
+    figure.clear()
+    title = f"Mount Tracking {dataIdString}, Azimuth = {azimuth:.1f}, Elevation = {elevation:.1f}"
+    plt.suptitle(title, fontsize=18)
+    # Azimuth axis
+    plt.subplot(3, 3, 1)
+    ax1 = az['azimuthCalculatedAngle'].plot(legend=True, color='red')
+    ax1.set_title("Azimuth axis", fontsize=16)
+    ax1.axvline(az.index[0], color="red", linestyle="--")
+    ax1.set_xticks([])
+    ax1.set_ylabel("Degrees")
+    plt.subplot(3, 3, 4)
+    plt.plot(times, az_error, color='red')
+    plt.title(f"Azimuth RMS error = {az_rms:.2f} arcseconds")
+    plt.ylim(-10.0, 10.0)
+    plt.xticks([])
+    plt.ylabel("Arcseconds")
+    plt.subplot(3, 3, 7)
+    ax7 = az_torque_1['azimuthMotor1Torque'].plot(legend=True, color='blue')
+    ax7 = az_torque_2['azimuthMotor2Torque'].plot(legend=True, color='green')
+    ax7.axvline(az.index[0], color="red", linestyle="--")
+    ax7.set_ylabel("Torque (motor current in amps)")
+
+    # Elevation axis
+    plt.subplot(3, 3, 2)
+    ax2 = el['elevationCalculatedAngle'].plot(legend=True, color='green')
+    ax2.set_title("Elevation axis", fontsize=16)
+    ax2.axvline(az.index[0], color="red", linestyle="--")
+    ax2.set_xticks([])
+    plt.subplot(3, 3, 5)
+    plt.plot(times, el_error, color='green')
+    plt.title(f"Elevation RMS error = {el_rms:.2f} arcseconds")
+    plt.ylim(-10.0, 10.0)
+    plt.xticks([])
+    plt.subplot(3, 3, 8)
+    ax8 = el_torque['elevationMotorTorque'].plot(legend=True, color='blue')
+    ax8.axvline(az.index[0], color="red", linestyle="--")
+    ax8.set_ylabel("Torque (motor current in amps)")
+
+    # Nasmyth2 rotator axis
+    plt.subplot(3, 3, 3)
+    ax3 = rot['nasmyth2CalculatedAngle'].plot(legend=True, color='blue')
+    ax3.set_title("Nasmyth2 axis", fontsize=16)
+    ax3.axvline(az.index[0], color="red", linestyle="--")
+    ax3.set_xticks([])
+    plt.subplot(3, 3, 6)
+    plt.plot(times, rot_error, color='blue')
+    plt.title(f"Nasmyth RMS error = {rot_rms:.2f} arcseconds")
+    plt.ylim(-100.0, 100.0)
+    plt.subplot(3, 3, 9)
+    ax9 = rot_torque['nasmyth2MotorTorque'].plot(legend=True, color='blue')
+    ax9.axvline(az.index[0], color="red", linestyle="--")
+    ax9.set_ylabel("Torque (motor current in amps)")
+    plt.savefig(saveFilename)
+
+    end = time.time()
+    elapsed = end-start
+    logger.debug(f"Elapsed time for plots = {elapsed}")
+
+    return True
---
+++ python/lsst/rapid/analysis/rubinTv.py
@@ -0,0 +1,323 @@
+# This file is part of rapid_analysis.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+from time import sleep
+from pathlib import Path
+import shutil
+import tempfile
+import logging
+import matplotlib.pyplot as plt
+
+try:
+    from google.cloud import storage
+    HAS_GOOGLE_STORAGE = True
+except ImportError:
+    HAS_GOOGLE_STORAGE = False
+
+try:
+    from lsst_efd_client import EfdClient
+    HAS_EFD_CLIENT = True
+except ImportError:
+    HAS_EFD_CLIENT = False
+
+import lsst.daf.persistence as dafPersist
+from lsst.pex.exceptions import NotFoundError
+from lsst.rapid.analysis.bestEffort import BestEffortIsr
+from lsst.rapid.analysis.imageExaminer import ImageExaminer
+from lsst.rapid.analysis.summarizeImage import SummarizeImage
+from lsst.rapid.analysis.mountTorques import plotMountTracking
+from lsst.rapid.analysis.monitorPlotting import plotExp
+from lsst.atmospec.utils import isDispersedDataId
+
+CHANNELS = ["summit_imexam", "summit_specexam", "auxtel_mount_torques",
+            "auxtel_monitor"]
+
+PREFIXES = {chan: chan.replace('_', '-') for chan in CHANNELS}
+
+
+def _dataIdToFilename(channel, dataId):
+    filename = f"{PREFIXES[channel]}_dayObs_{dataId['dayObs']}_seqNum_{dataId['seqNum']}.png"
+    return filename
+
+
+def _waitForDataProduct(butler, dataProduct, dataId, logger, maxTime=20):
+    cadence = 0.25
+    maxLoops = int(maxTime//cadence)
+    for retry in range(maxLoops):
+        if butler.datasetExists(dataProduct, dataId):
+            return butler.get(dataProduct, dataId)
+        else:
+            sleep(cadence)
+    logger.warn(f'Waited {maxTime}s for {dataProduct} for {dataId} to no avail')
+    return None
+
+
+class Uploader():
+    def __init__(self):
+        if not HAS_GOOGLE_STORAGE:
+            from lsst.rapid.analysis.utils import GOOGLE_CLOUD_MISSING_MSG
+            raise RuntimeError(GOOGLE_CLOUD_MISSING_MSG)
+        self.client = storage.Client()
+        self.bucket = self.client.get_bucket('rubintv_data')
+        self.log = logging.getLogger("googleUploader")
+
+    def googleUpload(self, channel, sourceFilename, uploadAsFilename=None):
+        if channel not in CHANNELS:
+            self.log.warn(f"Error: {channel} not in {CHANNELS}")
+            return
+
+        if uploadAsFilename and (os.path.basename(sourceFilename) != uploadAsFilename):
+            finalName = os.path.join(os.path.dirname(sourceFilename), uploadAsFilename)
+            shutil.move(sourceFilename, finalName)
+            assert os.path.exists(finalName)
+        else:
+            finalName = sourceFilename
+
+        path = Path(finalName)
+        blob = self.bucket.blob("/".join([channel, path.name]))
+        self.log.info(f'Uploaded {sourceFilename} to {finalName}')
+        blob.upload_from_filename(finalName)
+
+
+class Watcher():
+    cadence = 1  # in seconds
+
+    def __init__(self, repoDir, dataProduct, **kwargs):
+        """Watch for data products in a repo.
+        """
+        self.repoDir = repoDir
+        self.butler = dafPersist.Butler(repoDir)
+        self.dataProduct = dataProduct
+        self.log = logging.getLogger("watcher")
+
+    def _getLatestExpId(self):
+        return sorted(self.butler.queryMetadata(self.dataProduct, 'expId'))[-1]
+
+    def _getDayObsSeqNumFromExpId(self, expId):
+        return self.butler.queryMetadata(self.dataProduct, ['dayObs', 'seqNum'], expId=expId)[0]
+
+    def _getLatestImageDataIdAndExpId(self):
+        expId = self._getLatestExpId()
+        dayObs, seqNum = self._getDayObsSeqNumFromExpId(expId)
+        dataId = {'dayObs': dayObs, 'seqNum': seqNum}
+        return dataId, expId
+
+    def run(self, callback, durationInSeconds=-1):
+        """Run callback(dataId) each time a new image lands
+        """
+
+        if durationInSeconds == -1:
+            nLoops = int(1e9)
+        else:
+            nLoops = int(durationInSeconds//self.cadence)
+
+        lastFound = -1
+        for i in range(nLoops):
+            try:
+                dataId, expId = self._getLatestImageDataIdAndExpId()
+
+                if lastFound == expId:
+                    sleep(self.cadence)
+                    continue
+                else:
+                    callback(dataId)
+
+                lastFound = expId
+
+            except NotFoundError as e:  # NotFoundError when filters aren't defined
+                print(f'Skipped displaying {dataId} due to {e}')
+        return
+
+
+class IsrRunner():
+
+    def __init__(self, repoDir, **kwargs):
+        self.watcher = Watcher(repoDir, 'raw')
+        self.bestEffort = BestEffortIsr(repoDir, **kwargs)
+        outpath = os.path.join(repoDir, 'rerun/quickLook')
+        self.butler = dafPersist.Butler(outpath)
+        self.log = logging.getLogger("isrRunner")
+
+    def callback(self, dataId):
+        quickLookExp = self.bestEffort.getExposure(dataId)
+        self.butler.put(quickLookExp, 'quickLookExp', dataId)
+        self.log.info(f'Put quickLookExp for {dataId}, awaiting next image...')
+
+    def run(self):
+        self.watcher.run(self.callback)
+
+
+class ImExaminer():
+
+    def __init__(self, repoDir):
+        self.dataProduct = 'quickLookExp'
+        self.watcher = Watcher(repoDir, self.dataProduct)
+        self.uploader = Uploader()
+        self.repoDir = repoDir
+        self.butler = dafPersist.Butler(repoDir)
+        self.log = logging.getLogger("imExaminer")
+        self.channel = 'summit_imexam'
+
+    def _imExamine(self, exp, dataId, outputFilename):
+        if os.path.exists(outputFilename):  # unnecessary now we're using tmpfile
+            self.log.warn(f"Skipping {outputFilename}")
+            return
+        imexam = ImageExaminer(exp, savePlots=outputFilename, doTweakCentroid=True)
+        imexam.plot()
+
+    def callback(self, dataId):
+        try:
+            self.log.info(f'Running imexam on {dataId}')
+            tempFilename = tempfile.mktemp() + '.png'
+            uploadFilename = _dataIdToFilename(self.channel, dataId)
+            exp = _waitForDataProduct(self.butler, self.dataProduct, dataId, self.log)
+            if not exp:
+                raise RuntimeError(f'Failed to get {self.dataProduct} for {dataId}')
+            self._imExamine(exp, dataId, tempFilename)
+
+            self.log.info("Uploading imExam to storage bucket")
+            self.uploader.googleUpload(self.channel, tempFilename, uploadFilename)
+            self.log.info('Upload complete')
+
+        except Exception as e:
+            self.log.warn(f"Skipped imExam on {dataId} because {e}")
+            return None
+
+    def run(self):
+        self.watcher.run(self.callback)
+
+
+class SpecExaminer():
+
+    def __init__(self, repoDir):
+        self.dataProduct = 'quickLookExp'
+        self.watcher = Watcher(repoDir, self.dataProduct)
+        self.uploader = Uploader()
+        self.repoDir = repoDir
+        self.butler = dafPersist.Butler(repoDir)
+        self.log = logging.getLogger("specExaminer")
+        self.channel = 'summit_specexam'
+
+    def _specExamine(self, exp, dataId, outputFilename):
+        if os.path.exists(outputFilename):  # unnecessary now we're using tmpfile?
+            self.log.warn(f"Skipping {outputFilename}")
+            return
+        summary = SummarizeImage(exp, savePlotAs=outputFilename)
+        summary.run()
+
+    def callback(self, dataId):
+        try:
+            if not isDispersedDataId(dataId, self.butler):
+                self.log.info(f'Skipping non dispersed image {dataId}')
+                return
+
+            self.log.info(f'Running specExam on {dataId}')
+            tempFilename = tempfile.mktemp() + '.png'
+            uploadFilename = _dataIdToFilename(self.channel, dataId)
+            exp = _waitForDataProduct(self.butler, self.dataProduct, dataId, self.log)
+            if not exp:
+                raise RuntimeError(f'Failed to get {self.dataProduct} for {dataId}')
+            self._specExamine(exp, dataId, tempFilename)
+
+            self.log.info("Uploading specExam to storage bucket")
+            self.uploader.googleUpload(self.channel, tempFilename, uploadFilename)
+            self.log.info('Upload complete')
+
+        except Exception as e:
+            self.log.info(f"Skipped imExam on {dataId} because {e}")
+            return None
+
+    def run(self):
+        self.watcher.run(self.callback)
+
+
+class Monitor():
+    def __init__(self, repoDir):
+        self.dataProduct = 'quickLookExp'
+        self.watcher = Watcher(repoDir, self.dataProduct)
+        self.uploader = Uploader()
+        self.repoDir = repoDir
+        self.butler = dafPersist.Butler(repoDir)
+        self.log = logging.getLogger("monitor")
+        self.channel = 'auxtel_monitor'
+        self.fig = plt.figure(figsize=(12, 12))
+
+    def _plotImage(self, exp, dataId, outputFilename):
+        if os.path.exists(outputFilename):  # unnecessary now we're using tmpfile
+            self.log.warn(f"Skipping {outputFilename}")
+            return
+        plotExp(exp, dataId, self.fig, outputFilename)
+
+    def callback(self, dataId):
+        try:
+            self.log.info(f'Generating monitor image for {dataId}')
+            tempFilename = tempfile.mktemp() + '.png'
+            uploadFilename = _dataIdToFilename(self.channel, dataId)
+            exp = _waitForDataProduct(self.butler, self.dataProduct, dataId, self.log)
+            if not exp:
+                raise RuntimeError(f'Failed to get {self.dataProduct} for {dataId}')
+            self._plotImage(exp, dataId, tempFilename)
+
+            self.log.info("Uploading monitor image to storage bucket")
+            self.uploader.googleUpload(self.channel, tempFilename, uploadFilename)
+            self.log.info('Upload complete')
+
+        except Exception as e:
+            self.log.warn(f"Skipped monitor image for {dataId} because {e}")
+            return None
+
+    def run(self):
+        self.watcher.run(self.callback)
+
+
+class MountTorquePlotter():
+
+    def __init__(self, repoDir):
+        if not HAS_EFD_CLIENT:
+            from lsst.rapid.analysis.utils import EFD_CLIENT_MISSING_MSG
+            raise RuntimeError(EFD_CLIENT_MISSING_MSG)
+        self.dataProduct = 'raw'
+        self.watcher = Watcher(repoDir, self.dataProduct)
+        self.uploader = Uploader()
+        self.repoDir = repoDir
+        self.butler = dafPersist.Butler(repoDir)
+        self.client = EfdClient('summit_efd')
+        self.log = logging.getLogger("mountTorquePlotter")
+        self.channel = 'auxtel_mount_torques'
+        self.fig = plt.figure(figsize=(16, 16))
+
+    def callback(self, dataId):
+        try:
+            tempFilename = tempfile.mktemp() + '.png'
+            uploadFilename = _dataIdToFilename(self.channel, dataId)
+            plotted = plotMountTracking(dataId, self.butler, self.client, self.fig, tempFilename, 2, self.log)
+
+            if plotted:  # skips many image types and short exps
+                self.log.info("Uploading mount torque plot to storage bucket")
+                self.uploader.googleUpload(self.channel, tempFilename, uploadFilename)
+                self.log.info('Upload complete')
+
+        except Exception as e:
+            self.log.warn(f"Skipped creating mount plots for {dataId} because {e}")
+
+    def run(self):
+        self.watcher.run(self.callback)
